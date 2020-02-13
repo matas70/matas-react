97a2d2747ca1cc7481028c8d393300228547952b
Nir <99nirc99@gmail.com>
2020-02-13 13:35:53 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import {Headline} from "./headline/Headline";
+
+const App = () => {
+  
+  return (
+    <Headline></Headline>
+  );
+}
+
+export default App;
---
+++ src/headline/Headline.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Headline.module.css";
+
+export class Headline extends React.Component {
+    render() {
+        return (
+            <div className={styles.headline}>
+                
+            </div>
+        )
+    }
+}
